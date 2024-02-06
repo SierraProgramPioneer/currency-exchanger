8d894701ff0a77358a497f68e6202da9789bf752
Melissa Parker <147287059+SierraProgramPioneer@users.noreply.github.com>
2024-02-06 09:10:29 -0800

    Update example.test.js with sample and import statements

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -1,5 +1,9 @@
-describe("example test", () => {
-  test("should return true", () => {
-    expect(true).toEqual(true);
-  });
-});
+/* eslint-disable no-undef */
+import FILE from "../src/js/example.js";
+
+describe("CLASS", () => {
+
+    test("should do this", () => {
+        const thisObject = new Class(1);
+        expect(thisObject).toEqual(1);
+    });
