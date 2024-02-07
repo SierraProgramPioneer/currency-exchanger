e0292efcfa80aa1605a3e83d75fd1f4e2ecc3b80
SierraProgramPioneer <melissaparker520@gmail.com>
2024-02-07 12:58:15 -0800

    Start to add business logic for currency exchange API

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./css/styles.css";
+import ExchangeService from "./js/exchange-service";
 
 
+// Busines Logic
+
+function calculateExchange(dollarAmount, selectedCurrency) {
+    let exchangePromise = ExchangeService.exchangeDollars(dollarAmount, selectedCurrency);
+    exchangePromise.then(function (exchangeDataArray) {
+        printResult(exchangeDataArray);
+    }, function (errorArray) {
+        printError(errorArray);
+    });
+}
 
 
 // UI Logic
 
-
-function printResult(dollarAmount, selectedCurrency) {
-
-    document.getElementById("results").innerText = `Amount in selected currency is ${dollarAmount} ${selectedCurrency}`;
+function printResult(exchangeDataArray) {
+    document.getElementById("results").innerText = "result";
 }
+
+function printError(errorArray) {
+    document.getElementById("results").innerText = "error";
+}
+
 
 function clearPastResults() {
     document.getElementById("results").innerText = null;
@@ -23,7 +37,7 @@ function handleFormSubmission(event) {
     const dollarAmount = document.getElementById("usdAmount").value;
     const currencyOptions = document.getElementById("currencyOptions");
     const selectedCurrency = currencyOptions.value;
-    printResult(dollarAmount, selectedCurrency);
+    calculateExchange(dollarAmount, selectedCurrency);
     document.getElementById("usdAmount").value = null;
     currencyOptions.selectedIndex = 0;
 }
